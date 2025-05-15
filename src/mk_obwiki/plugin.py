ab4c369165646452404d153e74e9650df81e77c1
melkkarth <cervantes_0@hotmail.com>
2025-05-15 17:41:31 -0600

    Version 0.1.0

--- src/mk_obwiki/plugin.py
+++ src/mk_obwiki/plugin.py
@@ -1,7 +1,14 @@
+import logging
+import os
+import shutil
+
 from mkdocs.plugins import BasePlugin
 from mkdocs.config import config_options
 
-from mk_obwiki.utils import ObWikiParser
+from mk_obwiki.utils import ObsidianIconsImport
+from mk_obwiki.utils import MarkdownParser
+
+logger = logging.getLogger(__name__)
 
 class ObWikiPlugin(BasePlugin):
     """
@@ -9,6 +16,34 @@ class ObWikiPlugin(BasePlugin):
     """
     config_scheme = {}
 
-    def on_page_markdown(self, markdown: str, /, *, page, config, files) -> str | None:
-        parser = ObWikiParser()
+    def _assets_path(self, path):
+        return os.path.join(os.path.dirname(__file__), path)
+
+    def on_config(self, config):
+        # Inject custom CSS
+        config['extra_css'].append(f'stylesheets/extra.css')
+
+        # Inject template override path (partials)
+        override_dir = os.path.join(os.path.dirname(__file__), 'templates')
+        if os.path.isdir(override_dir):
+            config['theme'].dirs.insert(0, override_dir)
+
+        return config
+
+    def on_page_markdown(self, markdown: str, page, config, files):
+        parser = MarkdownParser()
         return parser.parse(markdown)
+
+    def on_nav(self, nav, config, files):
+        icons = ObsidianIconsImport(config)
+        for item in nav.items:
+            icons.apply_icon(item)
+        return nav
+
+    def on_post_build(self, config):
+        # Copy static files (CSS)
+        css_path = os.path.join(os.path.dirname(__file__), 'stylesheets', 'extra.css')
+        dest_dir = os.path.join(config['site_dir'], 'stylesheets')
+        os.makedirs(dest_dir, exist_ok=True)
+        shutil.copy(css_path, os.path.join(dest_dir, 'extra.css'))
+
--- src/mk_obwiki/utils.py
+++ src/mk_obwiki/utils.py
@@ -1,6 +1,15 @@
+import json
+import logging
+import os
 import re
+import shutil
 
-class ObWikiParser():
+from mkdocs.config.defaults import MkDocsConfig
+from mkdocs.structure.nav import Page, Section
+
+logger = logging.getLogger(__name__)
+
+class MarkdownParser():
     """
     Class to parse markdown and convert them 
     to mkdocs format.
@@ -9,15 +18,24 @@ class ObWikiParser():
     CODE_BLOCK_RE = r'^\s*```'
 
     def __init__(self):
-        pass
+        self._code_block_start = False
 
-    def _heading(self, line: str) -> str:
+    def _heading_parse(self, line: str) -> str:
         return '#' + line
 
-    def _code_block(self, line: str) -> str:
-        # Check for code block start and end if we want more updates
-        # on code block
-        return line.replace('ln:false', '')
+    def _code_block_parse(self, line: str) -> str:
+        # Each time a code block delimiter is found
+        # toggle code block start flag
+        new_line = line
+        self._code_block_start = not self._code_block_start
+        if self._code_block_start:
+            # Check for code block start and end if we want more updates
+            # on code block
+            if 'ln:false' in line:
+                new_line = line.replace('ln:false', '')
+            else:
+                new_line = line + ' linenums="1"'
+        return new_line
 
     def parse(self, markdown: str) -> str:
         """
@@ -28,10 +46,113 @@ class ObWikiParser():
         new_markdown = []
         for line in markdown.splitlines():
             if re.search(self.CODE_BLOCK_RE, line):
-                new_line = self._code_block(line)
+                new_line = self._code_block_parse(line)
             elif re.search(self.HEADING_RE, line):
-                new_line = self._heading(line)
+                new_line = self._heading_parse(line)
             else:
                 new_line = line
             new_markdown.append(new_line)
         return '\n'.join(new_markdown)
+
+class ObsidianIconsImport():
+    """
+    Class to import Obsidian icons to mkdocs.
+    """
+    PACKS_MAP = {
+        'Bo':   'boxicons',
+        'Co':   'coolicons',
+        'Fi':   'feather-icons',
+        'Fab':  'font-awesome-brands',
+        'Far':  'font-awesome-regular',
+        'Fas':  'font-awesome-solid',
+        'Ib':   'icon-brew',
+        'Li':   'lucide-icons',
+        'Oc':   'octicons',
+        'Ri':   'remix-icons',
+        'Ra':   'rpg-awesome',
+        'Si':   'simple-icons',
+        'Ti':   'tabler-icons'
+    }
+
+    def __init__(self, config: MkDocsConfig):
+        self._config = config
+        self._icons_path = os.path.join(
+            self._config['docs_dir'],
+            '.obsidian',
+            'icons'
+        )
+        icons_data = os.path.join(
+            self._config['docs_dir'],
+            '.obsidian',
+            'plugins',
+            'obsidian-icon-folder',
+            'data.json'
+        )
+        with open(icons_data, 'r', encoding='utf-8') as fd:
+            self._icons_map = json.load(fd)
+        self._section_chain = []
+
+    
+    def _get_svg(self, title:str):
+        icon_name = self._icons_map.get(title)
+        if not icon_name:
+            # Hack: Mkdocs capitilize section first letter, try to match title
+            # by force using all lowercase
+            logger.warning(
+                '[ObsidianIconsImport] "%s" Icon Not Found, search by FORCE',
+                title
+            )
+            for key in self._icons_map:
+                if key.lower() == title.lower():
+                    icon_name = self._icons_map.get(key)
+                    break
+            else:
+                logger.warning('[ObsidianIconsImport] "%s" FORCE search FAIL', title)
+                return None
+        
+        for key, value in self.PACKS_MAP.items():
+            if icon_name.startswith(key):
+                svg_name = icon_name.removeprefix(key) + '.svg'
+                svg_path = os.path.join(self._icons_path, value, svg_name)
+                break
+        else:
+            logger.warning('[ObsidianIconsImport] "%s" Pack Not Found', title)
+            return None
+
+        with open(svg_path, "r", encoding="utf-8") as fd:
+            svg = fd.read()
+
+        # Check if the SVG contains fill="none"
+        if 'fill="none"' in svg:
+            svg_class = "stroke-icon"
+        else:
+            svg_class = "fill-icon"
+
+        # Inject class into the <svg> tag
+        svg = svg.replace("<svg", f'<svg class="{svg_class}"', 1)
+        return svg
+
+
+    def apply_icon(self, item):
+        # Handle Page (single note)
+        if isinstance(item, Page):
+            md_name = item.file.src_path.removesuffix(self._config['docs_dir'])
+            page_name = os.path.splitext(os.path.basename(md_name))[0]
+            svg = self._get_svg(md_name)
+            if svg:
+                item.title = f'<span class="nav-icon">{svg}</span> {page_name}'
+
+        # Handle Section (folder)
+        elif isinstance(item, Section):
+            self._section_chain.append(item.title)
+            md_name = os.path.join(*self._section_chain)
+            svg = self._get_svg(md_name)
+            if svg:
+                item.title = f'<span class="nav-icon">{svg}</span> {item.title}'
+            
+            for child in item.children or []:
+                self.apply_icon(child)
+            self._section_chain.pop()
+
+
+
