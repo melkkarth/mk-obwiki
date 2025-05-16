7fe394c16ecf19736b78818e84c5ea9c0685895d
melkkarth <cervantes_0@hotmail.com>
2025-05-16 12:23:01 -0600

    Add package data to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import setup
 
 # Fake reference so GitHub still considers it a real package.
-setup(name="mk-obwiki")
+setup(
+    name="mk-obwiki",
+    version="0.1.0",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    package_data={
+        "mk_obwiki": [
+            "stylesheets/*.css",
+            "templates/partials/*.html",
+        ]
+    },
+    entry_points={
+        "mkdocs.plugins": [
+            "mk-obwiki = mk_obwiki.plugin:MyPlugin",
+        ]
+    },
+)
